5ad44e3712bc995cde53eb7db9c449d89c33a71b
pei-wang <wangpei9679@163.com>
2021-07-24 17:06:16 +1200

    add authorization

--- src/index.js
+++ src/index.js
@@ -2,52 +2,31 @@ const fs = require("fs");
 const path = require("path");
 const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
+const { getUserId } = require("./utils");
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 
 const prisma = new PrismaClient();
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: async (parent, args, context) => context.prisma.link.findMany(),
-    link: (parent, args) => links.find((item) => item.id === args.id),
-  },
-  Mutation: {
-    post: (parent, args, context) => {
-      const { url, description } = args;
-      const newLink = context.prisma.link.create({
-        data: {
-          url,
-          description,
-        },
-      });
-      return newLink;
-    },
-    deleteLink: (parent, args, context) => {
-      return prisma.link.delete({ where: { id: parseInt(args.id) } });
-    },
-    updateLink: (parent, args, context) => {
-      const { id, url, description } = args;
-
-      return context.prisma.link.update({
-        where: { id: parseInt(id) },
-        data: { url: url, description: description },
-      });
-    },
-  },
-
-  // Graphql will infer the resolver
-  // Link: {
-  //   id: (parent) => parent.id,
-  //   description: (parent) => parent.description,
-  //   url: (parent) => parent.url,
-  // },
+  Query,
+  Mutation,
+  User,
+  Link,
 };
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
-  context: {
-    prisma,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
   },
 });
 
